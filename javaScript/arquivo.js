f0a641874deed0e22860d8fbe997aeaee924f917
Rodrigo-Nery <rneribastos@hotmail.com>
2024-06-28 17:20:56 -0300

    inicio js

---
+++ javaScript/arquivo.js
@@ -0,0 +1,9 @@
+console.log("ola digão")
+/* 
+- let
+- const
+- var descontinuado / nao usado mais
+*/
+
+let manga = 40
+console.log (manga)
---
+++ javaScript/string.js
@@ -0,0 +1,15 @@
+/* 
+
+
+1 String -> textos 
+
+
+*/
+
+const texto1 ="textos com aspas duplas"
+const texto2 = 'textos com aspas somples'
+const texto3 = `textos com a crase `/*chamdos de Template literals ou temolate string */ 
+
+console.log (texto1)
+console.log (texto2)
+console.log (texto3)
