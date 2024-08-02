16f4c766ba102e0dda1775b4766c4b3def0168a0
Rodrigo-Nery <rneribastos@hotmail.com>
2024-08-02 00:51:21 -0300

    novos comit

---
+++ javaScript/functions.js
@@ -0,0 +1,19 @@
+/* 
+functions 
+
+* Um trecho de codigo so aparece quando e chamado 
+(famoso parente gente boa )
+*/
+
+const varialvel = "banana"
+ 
+console.log ( varialvel)
+
+function nomeNaTela () {
+    console.log ("Vem parente")
+
+}
+
+nomeNaTela()
+nomeNaTela()
+nomeNaTela()
